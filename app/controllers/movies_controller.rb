8ee87d3bce31e12ba4a1cd43e1a3747bcf754e50
Jacob <jacobamelse@hotmail.com>
2023-04-30 02:28:38 -0500

    commit tree 45954724ad2631db7b9c1e6cd8531b520a7bb0c0

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,6 @@
 class MoviesController < ApplicationController
+rescue_from ActiveRecord::RecordInvalid, with: :unprocessable_entity_error_message_handle
+
   wrap_parameters false
   
   def index
@@ -7,7 +9,7 @@ class MoviesController < ApplicationController
   end
 
   def create
-    movie = Movie.create(movie_params)
+    movie = Movie.create!(movie_params)
     render json: movie, status: :created
   end
 
@@ -15,6 +17,10 @@ class MoviesController < ApplicationController
 
   def movie_params
     params.permit(:title, :year, :length, :director, :description, :poster_url, :category, :discount, :female_director)
+  end
+
+  def unprocessable_entity_error_message_handle(invalid)
+    render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
   end
   
 end
